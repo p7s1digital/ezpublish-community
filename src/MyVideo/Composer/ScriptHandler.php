8d9d7809fe59c635c432af1aee29572a23ad303a
Patrik Jacoby <patrik.jacoby@magicinternet.de>
2013-12-02 14:33:09 +0100

    extend handling of the siteaccess config

--- src/MyVideo/Composer/ScriptHandler.php
+++ src/MyVideo/Composer/ScriptHandler.php
@@ -18,28 +18,34 @@ class ScriptHandler extends DistributionBundleScriptHandler
      */
     public static function generateSymLinks(CommandEvent $event)
     {
-        $options = self::getOptions( $event );
-        $webDir = $options['symfony-web-dir'];
         $baseDir = getcwd();
         $legacyVoid = $baseDir . '/ezpublish_legacy';
 
         // linking the extensions into the legacy void
-	$extensionList = array('mxdcommon','myvideo_admin','myvideo_message','myvideo_publish');
-        foreach( $extensionList as $extension) {
-	    if (false===file_exists($legacyVoid.'/extension/'.$extension)){ 
-	            symlink($baseDir.'/src/MyVideo/extension/'.$extension, $legacyVoid.'/extension/'.$extension);
-	    }
+        $extensionList = array('mxdcommon', 'myvideo_admin', 'myvideo_message', 'myvideo_publish');
+        foreach ($extensionList as $extension) {
+            if (false === file_exists($legacyVoid . '/extension/' . $extension)) {
+                symlink($baseDir . '/src/MyVideo/extension/' . $extension, $legacyVoid . '/extension/' . $extension);
+            }
         }
 
+        $siteaccess = $legacyVoid . '/settings/siteaccess';
         // linking the settings into the legacy void
-        // linking the settings into the legacy void
-        if (false === file_exists($legacyVoid . '/settings/siteaccess')) {
-            symlink($baseDir . '/ezpublish/config/legacy_settings/siteaccess', $legacyVoid . '/settings/siteaccess');
+        if (false === file_exists($siteaccess)) {
+            symlink($baseDir . '/ezpublish/config/legacy_settings/siteaccess', $siteaccess);
+        } else {
+            // if the original directory is given, we delete it and link our own settings here
+            if (true === is_dir($siteaccess)) {
+                exec('rm -rf ' . $siteaccess);
+                symlink(
+                    $baseDir . '/ezpublish/config/legacy_settings/siteaccess',
+                    $siteaccess
+                );
+            }
         }
         if (false === file_exists($legacyVoid . '/settings/override')) {
-            symlink($baseDir . '/ezpublish/config/legacy_settings/override',$legacyVoid . '/settings/override');
+            symlink($baseDir . '/ezpublish/config/legacy_settings/override', $legacyVoid . '/settings/override');
         }
-
     }
 
 } 
